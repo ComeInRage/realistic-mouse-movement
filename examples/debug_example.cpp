c2829451b2c673bcf9a29bf265cc82d42c6d0525
ComeInRage <montekek011@mail.ru>
2026-01-17 02:00:33 +0300

    Added debug example

---
+++ examples/debug_example.cpp
@@ -0,0 +1,33 @@
+#include "ticks_generator.hpp"
+
+#include <iostream>
+
+
+
+namespace rm = real_mouse;
+
+int main()
+{
+    using namespace std::chrono_literals;
+
+    static_assert(std::input_iterator<decltype(rm::ticks_generator{ 2ns }.begin()) > );
+
+    size_t iterations = 0;
+    size_t ticks_passed_in_10_iterations = 0;
+
+    for (auto ticks : rm::ticks_generator{ 2ns })
+    {
+        if (iterations == 10)
+        {
+            break;
+        }
+
+        ++iterations;
+
+        ticks_passed_in_10_iterations += ticks;
+    }
+
+    std::cout << std::format("Ticks passed in 10 iterations: {}\nEach tick is 2 ns.", ticks_passed_in_10_iterations) << std::endl;
+
+    return 0;
+}
